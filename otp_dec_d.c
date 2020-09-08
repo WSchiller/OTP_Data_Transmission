cd81352bd85558d8b1f1f8de5be63732b995f54a
WSchiller <62708205+WSchiller@users.noreply.github.com>
2020-09-07 21:24:22 -0400

    Update otp_dec_d.c

--- otp_dec_d.c
+++ otp_dec_d.c
@@ -30,10 +30,8 @@ void error(const char *msg) { perror(msg); exit(1); } // Error function used for
 void sendMessage(int socketFD, char* buffer, int msgLength) {
    int curMsgLength, charsRead;
    int charsWritten = 0;
-   // printf("Message length: %d\n", msgLength);
    int charsRemaining = msgLength;
    bool firstPass = true;
-
 
    char ackBuffer[100];
    memset(ackBuffer, '\0', 100);
@@ -49,21 +47,14 @@ void sendMessage(int socketFD, char* buffer, int msgLength) {
                 strncpy(tempBuffer, buffer + charsWritten, MAXSENDSIZE);
         }
         else {
-                strncpy(tempBuffer, buffer + charsWritten, charsRemaining);//strcat(tempBuffer,
+                strncpy(tempBuffer, buffer + charsWritten, charsRemaining);
                 tempBuffer[charsRemaining - 1] = '*';  // Delimiter to specify end of message
-         //       printf("* appended to end of message\n");
         }
         curMsgLength = strlen(tempBuffer);
-       // printf("%s\n", tempBuffer);
-       // printf("Chars Written: %d\n", charsWritten);
 
         charsWritten += send(socketFD, tempBuffer, curMsgLength, 0);
         charsRead = recv(socketFD, ackBuffer, sizeof(ackBuffer), 0);
-
-        //printf("Chars Read: %d\n", charsRead);
-        //printf("ACK Buffer: %s\n", ackBuffer);
         charsRemaining = msgLength - charsWritten;
-        //printf("Charms Remaining %d\n", charsRemaining);
    }
 }
 
@@ -75,17 +66,12 @@ void receiveMessage(int communicationFD, char* buffer) {
     bool firstPass = true;
     
     while(!newlineFound) {
-    // printf("Before received total bytes %d\n", totalBytesRead);
     	int numBytesRead = recv(communicationFD, tempBuffer, sizeof(tempBuffer), 0);
-    // printf("Num bytes read %d\n", numBytesRead);
-    // printf("Temp Buffer: %s\n", tempBuffer);
-
 	totalBytesRead += numBytesRead;
     	if (numBytesRead > 0) {	
 	   for (int i=0; i<numBytesRead; i++) {
 		char c = tempBuffer[i];
 		if (c == '*') {
-			// printf("* End of message!\n");
 			tempBuffer[i] = '\0';
 			newlineFound = true;
 			break;
@@ -93,7 +79,6 @@ void receiveMessage(int communicationFD, char* buffer) {
    	   }	   
 	   strcat(buffer, tempBuffer);   
 	}	
-
 	send(communicationFD, "Server has received message\n", 28, 0);
     }
 }
@@ -103,7 +88,6 @@ void generatePlaintext(int communicationFD) {
    
    char ciphertextBuffer[MAXSIZE], keyBuffer[MAXSIZE];
    int ciphertextLength;
-   // Clear buffers
    memset(ciphertextBuffer, '\0', MAXSIZE);
    memset(keyBuffer, '\0', MAXSIZE);
 
@@ -120,7 +104,6 @@ void generatePlaintext(int communicationFD) {
     // Generate Plaintext
     for (int i = 0; i < ciphertextLength; i++ ) {
     // Convert ASCII to 0 - 26 values.  0 = A, ' ' = 26
-    //	printf("Before %d  ",(int)ciphertextBuffer[i]);
     	if (ciphertextBuffer[i] == ' ') {
         	temp1 = 91;
      	}
@@ -135,12 +118,8 @@ void generatePlaintext(int communicationFD) {
    		temp2 = (int)keyBuffer[i];
  	}
    
-	// Decryption formula.   Subtract message and key, then modulus by size of character bank
-        // printf("cipher: %d key: %d ", temp1, temp2);
    	plaintextInt = (((temp1 - temp2) % 27) + 27) % 27;
-        // printf(" Plaintext: %d ", plaintextInt);
-   	plaintext[i] = plaintextInt + 65; // Store value
-        // printf("After %d  ", (int)plaintext[i]);
+   	plaintext[i] = plaintextInt + 65;
     }
    																									
    	sendMessage(communicationFD, plaintext, strlen(plaintext));   
@@ -156,7 +135,7 @@ int createListener(int portNumber) {
    serverAddress.sin_addr.s_addr = INADDR_ANY; // Any address is allowed for connection to this process
 
    // Set up the socket
-   int listenSocketFD = socket(AF_INET, SOCK_STREAM, 0); // Create the socket.  IPv4 family and reliable 2-way byte streaming	
+   int listenSocketFD = socket(AF_INET, SOCK_STREAM, 0); // Create the socket	
    if (listenSocketFD < 0) {
 	error("ERROR opening socket");
    }
@@ -164,8 +143,7 @@ int createListener(int portNumber) {
    // Enable the socket to begin listening.  Bind server address file to socket stored in file descriptor
    if (bind(listenSocketFD, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) { 
    	error("ERROR on binding");
-   }
-	
+   }	
    listen(listenSocketFD, 5); // Flip the socket on - it can now receive up to 5 connections at a time
    
    return listenSocketFD;
